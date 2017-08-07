9d53ae8e083fbe9f6b55628d7d45cfa43a144411
enamoria <phanhuykinh1@gmail.com>
2017-08-07 17:07:05 +0700

    Edit CONSTANT. Add ROOT_DIR definitin

--- CONSTANT.py
+++ CONSTANT.py
@@ -1,5 +1,15 @@
 """ Constant """
-GENKI4K_db_path = 'F:/Datasection/Learning_TensorFlow/TensorFlow.org_Tutorial/src/Emotion/GENKI4K/files/'
-GENKI4K_labels_path = 'F:/Datasection/Learning_TensorFlow/TensorFlow.org_Tutorial/src/Emotion/GENKI4K/'
-FLATTEN_SHAPE = 192*178*3
+import os
+# GENKI4K_db_path = 'F:/Datasection/Learning_TensorFlow/TensorFlow.org_Tutorial/src/Emotion/GENKI4K/files/'
+# GENKI4K_labels_path = 'F:/Datasection/Learning_TensorFlow/TensorFlow.org_Tutorial/src/Emotion/GENKI4K/'
+
+# This is your Project Root
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# OTHER PATH RELATED TO ROOT_DIR
+GENKI4K_db_path = ROOT_DIR + "/data/genki4k/files/"
+GENKI4K_labels_path = ROOT_DIR + "/data/genki4k/labels.txt"
+
+FLATTEN_SHAPE = 192 * 178 * 3
 IMAGE_SHAPE = [192, 178, 3]
+NUM_CLASS = 2
