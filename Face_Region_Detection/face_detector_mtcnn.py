339f5a8c008878f758b6dfd71d5d45c332f6e241
enamoria <phanhuykinh1@gmail.com>
2017-09-01 11:36:45 +0700

    Edit: pick the face with largest probability

--- Face_Region_Detection/face_detector_mtcnn.py
+++ Face_Region_Detection/face_detector_mtcnn.py
@@ -7,11 +7,13 @@ import os
 import sys
 import cv2
 import CONSTANT
+import numpy as np
 from Face_Region_Detection.mxnet_mtcnn_face_detection.mtcnn_detector import MtcnnDetector
 
 sys.path.append(CONSTANT.ROOT_DIR)
 
-genki4k_image_list = [f for f in os.listdir(CONSTANT.GENKI4K_db_path) if os.path.isfile(os.path.join(CONSTANT.GENKI4K_db_path, f))]
+genki4k_image_list = [f for f in os.listdir(CONSTANT.GENKI4K_db_path) if
+                      os.path.isfile(os.path.join(CONSTANT.GENKI4K_db_path, f))]
 
 face_detector = MtcnnDetector(model_folder="./mxnet_mtcnn_face_detection/model")
 
@@ -25,27 +27,42 @@ for image_name in genki4k_image_list:
             total_boxes = result[0]
             points = result[1]
 
-            # extract aligned face chips
-            chips = face_detector.extract_image_chips(img, points, 96, 0.37)
-            for i, chip in enumerate(chips):
-                # cv2.imshow('chip_' + str(i), chip)
-                # cv2.imwrite('./aligned_face/chip_' + str(i) + '.png', chip)
-                cv2.imwrite('./aligned_face/' + image_name, chip)
+            face_index = np.argmax(total_boxes[:, 4], axis=0)
 
-        print(image_name)
-
-            # draw = img.copy()
-            # for b in total_boxes:
-            #     cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
+            # print(face)
+            # # extract aligned face chips
+            # print(len(result))
             #
-            # for p in points:
-            #     for i in range(5):
-            #         cv2.circle(draw, (p[i], p[i + 5]), 1, (0, 0, 255), 2)
+            # print(total_boxes)
+            # print('-----------------------')
+            # print(points)
 
-            # cv2.imshow("detection result", draw)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
+
+            chips = face_detector.extract_image_chips(img, points, 96, 0.37)
+
+            ''' Pick the bigger (or with higher probability?) face '''
+            cv2.imwrite('./aligned_face_4/' + image_name, chips[face_index])
+            # for i, chip in enumerate(chips):
+            #     # cv2.imshow('chip_' + str(i), chip)
+            #     # cv2.imwrite('./aligned_face/chip_' + str(i) + '.png', chip)
+            #     cv2.imwrite('./aligned_face_2/' + image_name, chip)
+
+
+        # print(image_name)
+
+        # draw = img.copy()
+        # for b in total_boxes:
+        #     cv2.rectangle(draw, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (255, 255, 255))
+        #
+        # for p in points:
+        #     for i in range(5):
+        #         cv2.circle(draw, (p[i], p[i + 5]), 1, (0, 0, 255), 2)
+
+        # cv2.imshow("detection result", draw)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
     except Exception as e:
+        print(image_name)
         print("exception: ", e)
 
-    # cv2.waitKey(0)
+        # cv2.waitKey(0)
