e4e3aca256dcbc7f9ebc8c2b84de2aa0b1369cae
enamoria <phanhuykinh1@gmail.com>
2017-09-01 10:39:10 +0700

    CONSTANT

--- CONSTANT.py
+++ CONSTANT.py
@@ -8,11 +8,14 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # OTHER PATH RELATED TO ROOT_DIR
 GENKI4K_db_path = ROOT_DIR + "/data/genki4k/files/"
-GENKI4K_labels_path = ROOT_DIR + "/data/genki4k/labels.txt"
+GENKI4K_labels_path = ROOT_DIR + "/data/genki4k/labels.txt_modified"
+ALIGNED_CROPPED_db_path = ROOT_DIR + "/Face_Region_Detection/aligned_face_4/"
 
 # SOME INPUT DETAILS
 FLATTEN_SHAPE = 192 * 178 * 3
 IMAGE_SHAPE = [192, 178, 3]
 NUM_CLASS = 2
+
+NUM_FOLD = 4
 
 FC_NEURON = 160
